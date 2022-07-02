a593c2ef12e4edec2604493eb3d8fb742dcd034b
Chris Patenaude <patenauc@oregonstate.edu>
2022-07-02 08:46:45 -0700

    Use stitch instead of fabric

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,49 +2,50 @@
 import { fabric } from "fabric";
 import { useContext, useEffect, useRef } from "react";
 import GameCTX from "./GameCTX";
+import stitch from "./lib/stitch";
 
 // uncontrolled component implementation
 const Canvas = (props) => {
-  // set default props
-  const { setCanvas } = useContext(GameCTX);
-  const { children, className, style } = props;
-  const canvasElement = useRef(null);
-  const wrapperElement = useRef(null);
-  useEffect(() => {
-    if (!setCanvas) return;
-    const canvas = new fabric.Canvas(canvasElement.current, {
-      backgroundColor: "grey",
-      selection: false,
-      renderOnAddRemove: true,
-    });
+    // set default props
+    const { setCanvas } = useContext(GameCTX);
+    const { children, className, style } = props;
+    const canvasElement = useRef(null);
+    const wrapperElement = useRef(null);
+    useEffect(() => {
+        if (!setCanvas) return;
+        const canvas = stitch.canvas(canvasElement.current, {
+            backgroundColor: "grey",
+            selection: false,
+            renderOnAddRemove: true,
+        });
 
-    /**
-     * sets dimensions based on wrapper elements height and width
-     */
-    const setCanvasDimensions = () => {
-      canvas.setHeight(wrapperElement.current?.clientHeight || 0);
-      canvas.setWidth(wrapperElement.current?.clientWidth || 0);
-      canvas.renderAll();
-    };
+        /**
+         * sets dimensions based on wrapper elements height and width
+         */
+        const setCanvasDimensions = () => {
+            canvas.setHeight(wrapperElement.current?.clientHeight || 0);
+            canvas.setWidth(wrapperElement.current?.clientWidth || 0);
+            canvas.renderAll();
+        };
 
-    setCanvasDimensions();
-    // TODO: Not sure what the third arg is doing here
-    window.addEventListener("resize", setCanvasDimensions, false);
-    setCanvas(canvas);
+        setCanvasDimensions();
+        // TODO: Not sure what the third arg is doing here
+        window.addEventListener("resize", setCanvasDimensions, false);
+        setCanvas(canvas);
 
-    // cleanup event listeners and canvas object on dismount
-    return () => {
-      canvas.dispose();
-      window.removeEventListener("resize", setCanvasDimensions);
-    };
-  }, [setCanvas]);
+        // cleanup event listeners and canvas object on dismount
+        return () => {
+            canvas.dispose();
+            window.removeEventListener("resize", setCanvasDimensions);
+        };
+    }, [setCanvas]);
 
-  return (
-    <div ref={wrapperElement} className={className} style={style}>
-      <canvas ref={canvasElement} />
-      {children}
-    </div>
-  );
+    return (
+        <div ref={wrapperElement} className={className} style={style}>
+            <canvas ref={canvasElement} />
+            {children}
+        </div>
+    );
 };
 
 export default Canvas;
