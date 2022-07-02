7f6a9c45e16186fc5c8c56dc17db5fe23a404108
Chris Patenaude <patenauc@oregonstate.edu>
2022-07-02 07:03:30 -0700

    Format and fix code smells

--- src/Board.js
+++ src/Board.js
@@ -6,39 +6,40 @@ const LIGHT = "#c2856e";
 const DARK = "#664539";
 const COLUMNS = 8;
 const ROWS = 8;
+const SQUARE_SIZE = 80;
 
 const Board = () => {
-  const { canvas } = useContext(GameCTX);
-  const [board] = useState(() => {
-    return new Array(ROWS).fill(null).map((_, rowIndex) => {
-      return new Array(COLUMNS).fill("").map((_, colIndex) => {
-        const getColor = () => {
-          const rowIsEven = rowIndex % 2 === 0;
-          const columnIsEven = colIndex % 2 === 0;
-          if ((rowIsEven && columnIsEven) || (!rowIsEven && !columnIsEven)) {
-            return LIGHT;
-          }
-          if ((rowIsEven && !columnIsEven) || (!rowIsEven && columnIsEven)) {
-            return DARK;
-          }
-        };
+    const { canvas } = useContext(GameCTX);
+    const [board] = useState(() => {
+        return new Array(ROWS).fill(null).map((_row, rowIndex) => {
+            return new Array(COLUMNS).fill("").map((_column, colIndex) => {
+                const getColor = () => {
+                    const rowIsEven = rowIndex % 2 === 0;
+                    const columnIsEven = colIndex % 2 === 0;
+                    if ((rowIsEven && columnIsEven) || (!rowIsEven && !columnIsEven)) {
+                        return LIGHT;
+                    }
+                    if ((rowIsEven && !columnIsEven) || (!rowIsEven && columnIsEven)) {
+                        return DARK;
+                    }
+                };
 
-        return new fabric.Rect({
-          width: 100,
-          height: 100,
-          left: colIndex * 100,
-          top: rowIndex * 100,
-          fill: getColor(),
-          selectable: false,
+                return new fabric.Rect({
+                    width: SQUARE_SIZE,
+                    height: SQUARE_SIZE,
+                    left: colIndex * SQUARE_SIZE,
+                    top: rowIndex * SQUARE_SIZE,
+                    fill: getColor(),
+                    selectable: false,
+                });
+            });
         });
-      });
     });
-  });
 
-  useEffect(() => {
-    if (!canvas || !board.length) return;
-    // flatten multidimensional array and spread into canvas
-    canvas.add(...board.flat());
-  }, [canvas, board]);
+    useEffect(() => {
+        if (!canvas || !board.length) return;
+        // flatten multidimensional array and spread into canvas
+        canvas.add(...board.flat());
+    }, [canvas, board]);
 };
 export default Board;
--- src/Game.js
+++ src/Game.js
@@ -3,26 +3,26 @@ import Canvas from "./Canvas";
 import GameCTX, { GameState } from "./GameCTX";
 import Board from "./Board";
 
-const App = () => {
-  const [canvas, setCanvas] = useState(undefined);
-  const [gameState, setGameState] = useState(GameState.IDLE);
-  const [isMouseDown, setIsMouseDown] = useState(false);
+const Game = () => {
+    const [canvas, setCanvas] = useState(undefined);
+    const [gameState, setGameState] = useState(GameState.IDLE);
+    const [isMouseDown, setIsMouseDown] = useState(false);
 
-  return (
-    <GameCTX.Provider
-      value={{
-        isMouseDown,
-        setIsMouseDown,
-        gameState,
-        setGameState,
-        canvas,
-        setCanvas,
-      }}
-    >
-      <Canvas style={{ height: 800, width: 800 }}>
-        <Board />
-      </Canvas>
-    </GameCTX.Provider>
-  );
+    return (
+        <GameCTX.Provider
+            value={{
+                isMouseDown,
+                setIsMouseDown,
+                gameState,
+                setGameState,
+                canvas,
+                setCanvas,
+            }}
+        >
+            <Canvas style={{ height: 640, width: 640 }}>
+                <Board />
+            </Canvas>
+        </GameCTX.Provider>
+    );
 };
-export default App;
+export default Game;
