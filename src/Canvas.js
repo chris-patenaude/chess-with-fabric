2243e8ae24aaac8c32dfb9b3a8e2a438967b6039
Christopher Patenaude <patenauc@oregonstate.edu>
2022-07-03 13:01:22 -0700

    Implement event listener init function

--- src/Canvas.js
+++ src/Canvas.js
@@ -5,52 +5,67 @@ import stitch from "./lib/stitch";
 
 // uncontrolled component implementation
 const Canvas = (props) => {
-  // set default props
-  const { setCanvas, SQUARE_SIZE } = useContext(GameCTX);
-  const { children, className, style } = props;
-  const canvasElement = useRef(null);
-  const wrapperElement = useRef(null);
-  useEffect(() => {
-    if (!setCanvas) return;
-    const canvas = stitch.canvas(canvasElement.current, {
-      backgroundColor: "grey",
-      selection: false,
-      renderOnAddRemove: true,
-    });
+    // set default props
+    const { setCanvas, SQUARE_SIZE } = useContext(GameCTX);
+    const { children, className, style } = props;
+    const canvasElement = useRef(null);
+    const wrapperElement = useRef(null);
+    useEffect(() => {
+        if (!setCanvas) return;
+        const canvas = stitch.canvas(canvasElement.current, {
+            backgroundColor: "grey",
+            selection: false,
+            renderOnAddRemove: true,
+        });
 
-    /**
-     * sets dimensions based on wrapper elements height and width
-     */
-    const setCanvasDimensions = () => {
-      canvas.setHeight(wrapperElement.current?.clientHeight || 0);
-      canvas.setWidth(wrapperElement.current?.clientWidth || 0);
-      canvas.renderAll();
-    };
+        /**
+         * sets dimensions based on wrapper elements height and width
+         */
+        const setCanvasDimensions = () => {
+            canvas.setHeight(wrapperElement.current?.clientHeight || 0);
+            canvas.setWidth(wrapperElement.current?.clientWidth || 0);
+            canvas.renderAll();
+        };
 
-    setCanvasDimensions();
-    // TODO: Not sure what the third arg is doing here
-    window.addEventListener("resize", setCanvasDimensions, false);
-    canvas.on("mouse:up", (opt) => {
-      opt.target.set({
-        left: Math.round(opt.target.left / SQUARE_SIZE) * SQUARE_SIZE,
-        top: Math.round(opt.target.top / SQUARE_SIZE) * SQUARE_SIZE,
-      });
-    });
-    setCanvas(canvas);
+        setCanvasDimensions();
+        const initEventListeners = () => {
+            window.addEventListener("resize", setCanvasDimensions, false);
+            canvas.on("mouse:up", (opt) => {
+                const pointer = canvas.getPointer(opt.e);
+                const activePiece = opt.target;
+                if (!pointer || !activePiece) return;
+                // TODO: add check to see if square is occupied
+                // TODO: add check to see if enemy Piece
+                // TODO: add check to see if legal position
+                // TODO: add branch
+                // IF not legal move THEN return to origin
+                // IF not occupied THEN move to square
+                // IF is enemy THEN capture piece
+                // return to origin
+                const left =
+                    Math.round(activePiece.left / SQUARE_SIZE) * SQUARE_SIZE;
+                const top =
+                    Math.round(activePiece.top / SQUARE_SIZE) * SQUARE_SIZE;
+                activePiece.set({ left, top });
+            });
+        };
 
-    // cleanup event listeners and canvas object on dismount
-    return () => {
-      canvas.dispose();
-      window.removeEventListener("resize", setCanvasDimensions);
-    };
-  }, [setCanvas, SQUARE_SIZE]);
+        initEventListeners();
+        setCanvas(canvas);
 
-  return (
-    <div ref={wrapperElement} className={className} style={style}>
-      <canvas ref={canvasElement} />
-      {children}
-    </div>
-  );
+        // cleanup event listeners and canvas object on dismount
+        return () => {
+            canvas.dispose();
+            window.removeEventListener("resize", setCanvasDimensions);
+        };
+    }, [setCanvas, SQUARE_SIZE]);
+
+    return (
+        <div ref={wrapperElement} className={className} style={style}>
+            <canvas ref={canvasElement} />
+            {children}
+        </div>
+    );
 };
 
 export default Canvas;
